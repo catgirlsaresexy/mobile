37e3d2dd56ba4c217fe63b114b63a8776728fb62
JayLine <jaylineko@gmail.com>
2018-05-28 11:50:23 +0200

    Dump navigation library

--- app/src/main/java/sexy/catgirlsare/android/MainActivity.kt
+++ app/src/main/java/sexy/catgirlsare/android/MainActivity.kt
@@ -3,11 +3,14 @@ package sexy.catgirlsare.android
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment.findNavController
-import kotlinx.android.synthetic.main.main_activity.*
 import sexy.catgirlsare.android.api.setApiKey
+import sexy.catgirlsare.android.ui.main.MainFragment
+import sexy.catgirlsare.android.ui.uploads.UploadsFragment
 
 class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private lateinit var mainFragment: MainFragment
+    private lateinit var uploadsFragment: UploadsFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +22,22 @@ class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceCh
         onSharedPreferenceChanged(prefs, "key")
     }
 
-    override fun onSupportNavigateUp() = findNavController(content).navigateUp()
-
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
         if (key != "key") return
-        setApiKey(prefs.getString(key, ""))
-        findNavController(content).navigate(R.id.uploads)
+
+        val newKey = prefs.getString("key", "")
+        setApiKey(newKey)
+
+        if (newKey.isNullOrBlank()) {
+            mainFragment = MainFragment()
+            supportFragmentManager?.beginTransaction()
+                ?.replace(R.id.content, mainFragment)
+                ?.commit()
+        } else {
+            uploadsFragment = UploadsFragment()
+            supportFragmentManager?.beginTransaction()
+                ?.replace(R.id.content, uploadsFragment)
+                ?.commit()
+        }
     }
 }
