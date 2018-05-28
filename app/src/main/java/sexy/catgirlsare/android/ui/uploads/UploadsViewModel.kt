f7c06887620ec832fe40466b2925876bfd86ba6f
JayLine <jaylineko@gmail.com>
2018-05-28 12:35:05 +0200

    Disable placeholders again

--- app/src/main/java/sexy/catgirlsare/android/ui/uploads/UploadsViewModel.kt
+++ app/src/main/java/sexy/catgirlsare/android/ui/uploads/UploadsViewModel.kt
@@ -11,7 +11,7 @@ class UploadsViewModel : ViewModel() {
         .setPageSize(50)
         .setInitialLoadSizeHint(50)
         .setPrefetchDistance(20)
-        .setEnablePlaceholders(true)
+        .setEnablePlaceholders(false)
         .build()
 
     private val source = UploadsDataSource.Factory()
